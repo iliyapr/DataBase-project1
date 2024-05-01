f9728a9bf777d61bd33c28ad95e6797881dfd3b2
Emad <emdhdri@gmail.com>
2024-05-01 19:00:51 +0330

    Add customers_with_the_most_orders view

--- views.sql
+++ views.sql
@@ -11,6 +11,25 @@ JOIN Item AS i
 GROUP BY o.ordered_at
 ORDER BY o.ordered_at DESC
 
+GO
 
+CREATE VIEW customers_with_the_most_orders
+AS
+SELECT 
+    GetFullName(c.first_name, c.last_name), 
+    total_paid, 
+    total_orders
+FROM Customer AS c 
+JOIN
+(SELECT TOP 1
+	o.customer_id AS c_id, SUM(i.price * oi.amount) AS total_paid, COUNT(*) AS total_orders
+FROM [Order] AS o
+JOIN Order_Item AS oi
+    ON oi.order_id = o.id
+JOIN Item AS i
+    ON i.id = oi.item_id
+GROUP BY o.customer_id
+ORDER BY total_orders DESC) AS max_orders
+ON max_orders.c_id = c.id 
 
 
