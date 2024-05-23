7cc95d0124c5c3498b7fa5f31cb263802573952b
Sharmaann <shakerian.arman123@gmail.com>
2024-05-23 15:09:41 +0330

    feat: add employees app to INSTALLED_APPS + set MSSQL as DB

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': BASE_DIR / 'db.sqlserver',
     }
 }
 
