b6a6ef70057faf3afff27b9529884b3269de4700
Sharmaann <shakerian.arman123@gmail.com>
2024-05-23 14:53:38 +0330

    django-templates

--- restaurant/employees/views.py
+++ restaurant/employees/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .forms import EmployeeForm
+
+from models import Employee
 # Create your views here.
+
+def create_employee(request, ):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+                form.save()
+                return HttpResponse('Employee created')
+    else:
+        return HttpResponse('Method not allowed', status=400)
+    ...
