7a756ba2f7d085acca9fbe9322bd4e8a21d55970
Sharmaann <shakerian.arman123@gmail.com>
2024-05-24 20:12:10 +0330

    feat: add CRUD views for chef and employee

--- Phase2/restaurant/Employees/views.py
+++ Phase2/restaurant/Employees/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from rest_framework import generics
+
+from .models import Chef, Employee
+
+from .serializers import ChefSerializer, EmployeeSerializer
+
+class EmployeeRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    
+    def get_object(self):
+        ssn = self.kwargs['ssnPK']
+        return get_object_or_404(Employee, ssn=ssn)
+
+class EmployeeListCreate(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class ChefRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chef.objects.all()
+    serializer_class = ChefSerializer
+    
+    def get_object(self):
+        ssn = self.kwargs['ssnPK']
+        employee = get_object_or_404(Employee, ssn=ssn)
+        return get_object_or_404(Chef, ssn=employee)
+
+class ChefListCreate(generics.ListCreateAPIView):
+    queryset = Chef.objects.all()
+    serializer_class = ChefSerializer
