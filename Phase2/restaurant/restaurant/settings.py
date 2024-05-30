0f321dbbbd79585a082f43b743d6c1de21df0a1d
Sharmaann <shakerian.arman123@gmail.com>
2024-05-30 17:42:38 +0330

    set SECRET_KEY manually

--- Phase2/restaurant/restaurant/settings.py
+++ Phase2/restaurant/restaurant/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET")
+SECRET_KEY = 'django-insecure-^x)74_sj-0*i5*k#q0jpp$(cw49c9l$=ub+siqfl#t*2rc#x^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
