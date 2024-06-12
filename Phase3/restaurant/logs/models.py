35c9f23b02d7943ac047a76d0e5378bc60249702
mohammadsaeedarvenaghi <mohammadsaeedarvenaghi@gmail.com>
2024-06-12 19:04:21 +0330

    Add view.sql apis

--- Phase3/restaurant/logs/models.py
+++ Phase3/restaurant/logs/models.py
@@ -30,3 +30,159 @@ class UpdateLog(models.Model):
     class Meta:
         db_table = 'UpdateLog'
         managed = False
+
+from django.db import models
+
+class DailyInsertLog(models.Model):
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    changed_data = models.TextField()
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_DailyInsertLog'
+
+
+class DailyDeleteLog(models.Model):
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    changed_data = models.TextField()
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_DailyDeleteLog'
+
+
+class DailyUpdateLog(models.Model):
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    previous_data = models.TextField()
+    changed_data = models.TextField()
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_DailyUpdateLog'
+
+
+class LogEmployee(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Employee'
+
+class LogChef(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Chef'
+
+class LogWaiter(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Waiter'
+
+class LogShipper(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Shipper'
+
+class LogManager(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Manager'
+
+class LogCustomer(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Customer'
+
+class LogOrder(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Order'
+
+class LogItem(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Item'
+
+class LogRecipe(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Recipe'
+
+class LogIngredient(models.Model):
+    change_type = models.CharField(max_length=10)
+    log_id = models.IntegerField(primary_key=True)
+    table_name = models.CharField(max_length=255)
+    data = models.TextField()
+    previous_data = models.TextField(null=True)
+    changed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'vw_Log_Ingredient'
